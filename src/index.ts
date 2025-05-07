99463e467047ab55cfad7ff581ce73230e04c4bf
Pandey-Krishnaa <krish36pandey@gmail.com>
2025-05-07 18:57:50 +0530

    fixed

--- src/index.ts
+++ src/index.ts
@@ -4,50 +4,27 @@ import qr from "qrcode-terminal";
 import dotenv from "dotenv";
 
 import cors from "cors";
-import db from "./db";
-dotenv.config();
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+import initSqlite from "@sqlite.org/sqlite-wasm";
 
-// Get batches with optional filter
-app.post("/batches", (req, res) => {
-  const sql = `INSERT INTO batches (id, name, status) VALUES (?, ?, ?)`;
-  const params = ["qwkdjnqxkjqw", "krishna", "nuasjn"];
-  db.run(sql, params, function (err) {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.status(201).json({ id: this.lastID });
-  });
-});
-app.get("/batches", (req, res) => {
-  const { status } = req.query;
-  let sql = `SELECT * FROM batches`;
-  const params = [];
+async function main() {
+  const sqlite3 = await initSqlite();
+  const db = new sqlite3.oo1.DB();
 
-  if (status) {
-    sql += ` WHERE status = ?`;
-    params.push(status);
+  db.exec(`CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL
+  )`);
+
+  db.exec(`INSERT INTO todos (title) VALUES ('First Task')`);
+
+  const stmt = db.prepare("SELECT * FROM todos");
+
+  while (stmt.step()) {
+    const row = stmt.get({});
+    console.log(row); // { id: 1, title: 'First Task' }
   }
 
-  db.all(sql, params, (err, rows) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.json(rows);
-  });
-});
-const server = app.listen(9090, "0.0.0.0", () => {
-  const addressInfo = server.address();
-  if (typeof addressInfo === "object" && addressInfo?.port) {
-    const localIp = ip.address();
-    const url = `http://${localIp}:${addressInfo.port}`;
+  db.close();
+}
 
-    console.log(`\n✅ Server running at ${url}`);
-    console.log("📱 Scan the QR code below to access it:\n");
-
-    qr.generate(url, { small: true });
-  } else {
-    console.error("❌ Could not determine server address");
-  }
-});
+main().catch(console.error);
